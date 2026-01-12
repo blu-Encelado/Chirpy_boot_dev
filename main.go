630b65b14090d272afe6db8ce7f0b40827701d26
blu-Encelado <bluencelado@gmail.com>
2026-01-12 09:26:47 +0100

    lessons 7-13

--- main.go
+++ main.go
@@ -1,26 +1,29 @@
 package main
 
 import (
-	"fmt"
+	"log"
 	"net/http"
-	"os"
 )
 
-const path string = "/"
+const filepathroot string = "."
+const port string = "8080"
 
 func main() {
 	serv_mux := http.NewServeMux()
-	serv_mux.Handle(path, http.FileServer(http.Dir(".")))
-	server := http.Server{
-		Addr:                         ":8080",
-		Handler:                      serv_mux,
-		DisableGeneralOptionsHandler: true,
+
+	serv_mux.Handle("/app/", http.StripPrefix("/app", http.FileServer(http.Dir(filepathroot))))
+	serv_mux.HandleFunc("/healthz", handlerReadiness)
+
+	server := &http.Server{
+		Addr:    ":" + port,
+		Handler: serv_mux,
 	}
 
-	err := server.ListenAndServe()
-
-	if err != nil {
-		fmt.Println("fail to start the server")
-		os.Exit(1)
-	}
+	log.Printf("Serving files from %s on port: %s\n", filepathroot, port)
+	log.Fatal(server.ListenAndServe())
+}
+func handlerReadiness(w http.ResponseWriter, r *http.Request) {
+	w.Header().Add("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte(http.StatusText(http.StatusOK)))
 }

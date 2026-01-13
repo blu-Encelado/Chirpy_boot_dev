b38cc3aa953c3e434969bd1bb0cfe0aaab35332a
blu-Encelado <bluencelado@gmail.com>
2026-01-13 09:50:42 +0100

    lessons 1-5 (routing)

--- main.go
+++ main.go
@@ -1,29 +1,61 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
+	"sync/atomic"
 )
 
-const filepathroot string = "."
-const port string = "8080"
-
 func main() {
-	serv_mux := http.NewServeMux()
+	const filepathRoot = "."
+	const port = "8080"
 
-	serv_mux.Handle("/app/", http.StripPrefix("/app", http.FileServer(http.Dir(filepathroot))))
-	serv_mux.HandleFunc("/healthz", handlerReadiness)
+	serv_mux := http.NewServeMux()
+	apiCfg := apiConfig{}
+
+	serv_mux.Handle("/app/", apiCfg.middlewareMetricsInc(http.StripPrefix("/app", http.FileServer(http.Dir(filepathRoot)))))
+	serv_mux.HandleFunc("GET /healthz", handlerReadiness)
+	serv_mux.HandleFunc("GET /metrics", apiCfg.handleCountHits)
+	serv_mux.HandleFunc("POST /reset", apiCfg.handleResetCountHits)
 
 	server := &http.Server{
 		Addr:    ":" + port,
 		Handler: serv_mux,
 	}
 
-	log.Printf("Serving files from %s on port: %s\n", filepathroot, port)
+	log.Printf("Serving files from %s on port: %s\n", filepathRoot, port)
 	log.Fatal(server.ListenAndServe())
 }
 func handlerReadiness(w http.ResponseWriter, r *http.Request) {
 	w.Header().Add("Content-Type", "text/plain; charset=utf-8")
 	w.WriteHeader(http.StatusOK)
 	w.Write([]byte(http.StatusText(http.StatusOK)))
+}
+
+type apiConfig struct {
+	fileserverHits atomic.Int32
+}
+
+func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
+
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		cfg.fileserverHits.Add(1)
+		next.ServeHTTP(w, r)
+	})
+
+}
+
+func (cfg *apiConfig) handleCountHits(w http.ResponseWriter, r *http.Request) {
+	w.Header().Add("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	string_var := fmt.Sprintf("Hits: %d", cfg.fileserverHits.Load())
+	w.Write([]byte(string_var))
+}
+
+func (cfg *apiConfig) handleResetCountHits(w http.ResponseWriter, r *http.Request) {
+	w.Header().Add("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	cfg.fileserverHits.Swap(0)
+	w.Write([]byte("Hits Resetted"))
 }
